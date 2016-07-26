3ead7bddfe4fbedf9fc3a77b4b0a8d38289c1060
SnowWalkerJ <jike3212001@163.com>
2016-07-26 13:39:11 +0800

    Markdown增加列表功能

--- Markdown/Markdown.hs
+++ Markdown/Markdown.hs
@@ -26,9 +26,9 @@ module Markdown where
 
     headParser::CharParser () String
     headParser = do
-        --try spaces
+        many (char ' ')
         sharps <- many1 (char '#')
-        --try space
+        try (char ' ') <|> return ' '
         let n = length sharps
         content <- contentParser
         return $ "<h" ++ show n ++"><b>" ++ content ++ "</b></h" ++ show n ++ ">\n"
@@ -40,13 +40,45 @@ module Markdown where
     parser = do 
         parsed <- flip sepEndBy (char '\n') $ 
             try hrParser 
+            <|> try (ulParser 0)
             <|> try headParser 
             <|> try paragraphParser
             <|> return ""
         return $ concat parsed
 
-    hrParser = do
-        s <- many (char '-')
-        if length s > 5 then return "<hr />\n"
+    hrParser = atLeast 5 (char '-') >> return "<hr />\n"
+
+    atLeast n ps = do
+        s <- many ps
+        if length s >= n then return s
         else fail ""
-    
+
+    countParser parser = do
+        s <- many parser
+        return $ length s
+
+    countParser1 parser = do
+        s <- many1 parser
+        return $ length s
+
+    ulParser::Int->CharParser () String
+    ulParser upLevel = do
+        n <- countParser (char '\t')
+        if n < upLevel then fail "" else char '-'
+        many $ oneOf " \t"
+        thisItem <- contentParser
+        char '\n'
+        items <- many (try (liParser n) <|> try(ulParser n)) <|> return [""]
+        let indent = replicate n '\t'
+            itemHTML = indent ++ "\t<li>" ++ thisItem ++ "</li>\n"
+        return $ indent ++ "<ul>\n" ++ itemHTML ++ concat items ++ indent ++ "</ul>\n"
+
+    liParser n = do
+        string $ replicate n '\t'
+        char '-'
+        many (oneOf " \t")
+        item <- contentParser
+        char '\n'
+        let indent = replicate (n+1) '\t'
+        return $ indent ++ "<li>" ++ item ++ "</li>\n"
+
